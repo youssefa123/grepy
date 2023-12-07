573f2b8bfabc362cb7876469a3c96a98fba416cd
youssefa123 <yabdelhady905@gmail.com>
2023-12-06 19:48:10 -0500

    Working on DFA to read text file outputting in DOT format

--- src/dfa.py
+++ src/dfa.py
@@ -33,4 +33,50 @@ class DFA:
                 return False  # If no transition exists for the input char, reject the string
         return current_state in self.accept_states
     
-    
+# Function to test strings from the file with the DFA
+def test_file_with_dfa(file_path, dfa):
+    results = {} #set the result to an empty array 
+    with open(file_path, 'r') as file: 
+        for line in file: 
+            cleaned_line = line.strip() #Strip white
+            results[cleaned_line] = dfa.is_accepted(cleaned_line)
+    return results
+
+if __name__ == '__main__':
+    dfa = DFA()
+
+    #Using 0,1 for the regex (0+1)*1
+    dfa.add_alphabet('0')
+    dfa.add_alphabet('1')
+
+    #DFA states 
+    dfa.add_states(0)
+    dfa.add_states(1)
+
+    #Start state 
+    dfa.set_start_state(0)
+
+    #Accept states
+    dfa.set_accept_state(0)  
+    dfa.set_accept_state(1)
+
+    #Transitions 
+    dfa.add_transition(0, '0', 0)
+    dfa.add_transition(0, '1', 1)
+    dfa.add_transition(1, '0', 0)
+    dfa.add_transition(1, '1', 1)
+
+    file_path = '../data/inputFile2.txt'
+
+    results_from_file = test_file_with_dfa(file_path, dfa)
+    for line, result in results_from_file.items():
+        print(f"'{line}': {'Accepted' if result else 'Rejected'}")
+
+    dot = dfa.to_dot()
+    output_path = '../output/dfa_output'
+
+    # Save the DOT file
+    dot.render(output_path, format='dot', view=False)
+
+    # Print to confirm the DOT file has been generated
+    print(f"The DOT representation has been saved to {output_path}.dot")
