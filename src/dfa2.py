616a00c82a127de11e4f1afc4e86b25216106acc
youssefa123 <yabdelhady905@gmail.com>
2023-12-07 17:50:12 -0500

    Made new dfa file and set the Made new DFA file and set the transition logic for regex 'a+b*'. Implemented state transitions, acceptance criteria

--- src/dfa2.py
+++ src/dfa2.py
@@ -1,2 +1,26 @@
 from graphviz import Digraph
 
+class DFA:
+    def __init__(self):
+        self.transitions = {}
+        self.current_state = 0
+        self.accept_states = set()
+        
+    # define the transition logic
+    def add_transition(self, state, input_char, next_state):
+        if state not in self.transitions:
+            self.transitions[state] = {}
+        self.transitions[state][input_char] = next_state
+
+    #define the accept state logic.
+    def set_accept_state(self, state):
+        self.accept_states.add(state)
+
+    #resets the state to current 
+    def reset(self):
+        self.current_state = 0
+   
+    #Transition logic 
+    def transition(self, input_char):
+        if input_char in self.transitions[self.current_state]:
+            self.current_state = self.transitions[self.current_state][input_char]
