fcd139831dec4cb4a6ea0380f54f29a48d64db95
youssefa123 <yabdelhady905@gmail.com>
2023-12-07 18:09:47 -0500

    Finished adding the logic for the new dfa

--- src/dfa2.py
+++ src/dfa2.py
@@ -24,3 +24,28 @@ class DFA:
     def transition(self, input_char):
         if input_char in self.transitions[self.current_state]:
             self.current_state = self.transitions[self.current_state][input_char]
+
+    def is_accepted(self, input_string):
+        #reset once we get to accepting state 
+        self.reset()
+        for char in input_string:
+            self.transition(char)
+        return self.current_state in self.accept_states
+    
+    def to_dot(self):
+        dot = Digraph()
+        # check every state in the DFA
+        for state in self.transitions:
+            # If the state is an accept state, represent it as a double circle, else represent it as a circle 
+            shape = "doublecircle" if state in self.accept_states else "circle"
+            dot.node(str(state), shape = shape)
+            
+            #transition between states in the DFA 
+            for input_char, next_state in self.transitions[state].items():
+                dot.edge(str(state), str(next_state), label=input_char)
+        
+        return dot
+    
+if __name__ == '__main__':
+    dfa = DFA()
+    
