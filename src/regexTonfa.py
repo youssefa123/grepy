418433a935c59ac90ed8088af7889b34e7f29793
youssefa123 <yabdelhady905@gmail.com>
2023-12-08 01:15:46 -0500

    Got regex to NFA to work, bugs

--- src/regexTonfa.py
+++ src/regexTonfa.py
@@ -8,6 +8,19 @@ class NFAState:
         # transition for a character
         self.state_transitions.setdefault(transition_char, set()).add(next_state)
     
+    def compute_epsilon_closure(self):
+        closure_set = {self}  # Initialize closure set with the state itself
+        search_stack = [self]  # Stack for depth-first search in the closure set
+
+        while search_stack:
+            current_state = search_stack.pop()  # Retrieve a state from the stack
+            # Process epsilon transitions (None)
+            for next_state in current_state.state_transitions.get(None, []):
+                if next_state not in closure_set:
+                    closure_set.add(next_state)  # Add state to closure set
+                    search_stack.append(next_state)  # Append state for further exploration
+
+        return closure_set  # Return the computed epsilon closure
     
 #create NFA from regex Initialized start, current and accept state
 def regex_to_nfa(regex):
@@ -15,34 +28,26 @@ def regex_to_nfa(regex):
     current_states = [start_state]
     accept_state = NFAState("accept", final_state=True)
 
-    #index for checking through characters in Regex.
     i = 0
-    
-    # Iterate through the characters in the regex
     while i < len(regex):
         char = regex[i]
 
-        #Check if the character is a '^'
         if char == '^':
-            continue  # Skip '^' character
-        
-        # Check if the character is a '$'
+            i += 1  # Skip '^' character and continue
+            continue
+
         elif char == '$':
-            # Add transitions from current states to the accept state
             for state in current_states:
                 state.define_transition(None, accept_state)
-        
-        elif char == '|':
-            # Create a new state for alternate paths
-            new_state = NFAState()
 
-            # transitions from current state to the new state
+        elif char == '|':
+            new_state = NFAState()
             for state in current_states:
                 state.define_transition(None, new_state)
             current_states = [new_state]
             i += 1  # Skip the next character
+
         else:
-            #if the character is not a special, make new states for each current state and add transitions
             new_states = []
             for state in current_states:
                 new_state = NFAState(char)
@@ -51,10 +56,33 @@ def regex_to_nfa(regex):
             current_states = new_states
 
         i += 1
-    # transitions from the final current states to the accept state
+
     for state in current_states:
         state.define_transition(None, accept_state)
 
-    return start_state
+    return start_state  # This should be outside the while loop
 
 
+def simulate_nfa(nfa, test_input):
+    current_states = nfa.compute_epsilon_closure()
+    for char in test_input:
+        next_states = set()
+        for state in current_states:
+            next_states.update(state.state_transitions.get(char, []))
+        current_states = set()
+        for state in next_states:
+            current_states.update(state.compute_epsilon_closure())
+
+    return any(state.final_state for state in current_states)
+
+def main():
+    regex = input("Enter a regular expression: ")
+    nfa = regex_to_nfa(regex)
+
+    test_inputs = ["ab"]  # Sample test inputs
+    for test_input in test_inputs:
+        result = "Accepted" if simulate_nfa(nfa, test_input) else "Rejected"
+        print(f"Input: {test_input}, Result: {result}")
+
+if __name__ == "__main__":
+    main()
